6a4144541de40fdf03a12af9bd0563e8e4aa0160
renzheng11 <renzheng112@gmail.com>
2024-03-08 01:44:34 -0500

    typeface

--- src/Home.js
+++ src/Home.js
@@ -34,85 +34,87 @@ export function Home() {
 				<div className="burger-white-bg"></div>
 				<div className="content">
 					<div className="wrapper">
-						<div className="left-side">
-							<div className="top">
-								<div className="nav">
-									<div className="brand">
-										<span>Ren Zheng</span>
-										{/* <img
-											src={require("../src/Assets/icons/logo.png")}
-											alt="project"
-										/> */}
-									</div>
+						<div className="navParent">
+							<div className="topbar"></div>
+							<div className="nav">
+								<div className="brand">
+									<span>REN ZHENG</span>
+									{/* <img
+												src={require("../src/Assets/icons/logo.png")}
+												alt="project"
+											/> */}
 								</div>
-								<div className="landing">
-									<div className="headline-name">
-										Hello, I'm Ren <span className="pronouns">(they/them)</span>
-										. I'm a designer and developer studying and living in
-										Atlanta.
-									</div>
-									<div className="headline-text">
-										My focus is on crafting&nbsp;
-										<span id="memory">memorable experiences</span> through user
-										research, visual design, and storytelling.
-									</div>
-								</div>
-							</div>
-							<div className="bottom">
-								<div className="links">
-									<div className="link">
-										<div className="link-logo">
-											<img
-												src={require("../src/Assets/icons/link-logo.png")}
-												alt="project"
-											/>
-										</div>
-										<a href="https://drive.google.com/file/d/1v16MdAD0AdDj4YeK8HdwukBeRNUGvVDZ/view?usp=drive_link">
-											Resume
-										</a>
-									</div>
-									<div className="link">
-										<div className="link-logo">
-											<img
-												src={require("../src/Assets/icons/link-logo.png")}
-												alt="project"
-											/>
-										</div>
-										<a href="https://www.linkedin.com/in/renzheng/">LinkedIn</a>
-									</div>
-									<div className="link">
-										<div className="link-logo">
-											<img
-												src={require("../src/Assets/icons/link-logo.png")}
-												alt="project"
-											/>
-										</div>
-										<a href="https://github.com/renzheng11">Github</a>
-									</div>
-									<div className="link">
-										<div className="link-logo">
-											<img
-												src={require("../src/Assets/icons/link-logo.png")}
-												alt="project"
-											/>
-										</div>
-										<a href="https://www.are.na/ren-zheng/channels">Are.na</a>
-									</div>
+								<div className="work">
+									<span>WORK</span>
 								</div>
 							</div>
 						</div>
-						<div className="right-side">
-							<div className="right-wrapper">
-								<div className="work">
-									<span>Work</span>
+						<div class="side-wrapper">
+							<div className="left-side">
+								<div className="landing">
+									{/* <span className="pronouns">(they/them)</span> */}
+									<div className="headline-name">
+										A multidisciplinary designer and developer focused on
+										crafting&nbsp;
+										<span id="memory">memorable experiences</span> through
+										research, visual design, and storytelling.
+									</div>
 								</div>
-								{/* <div id="project-destination"></div> */}
-								<div className="all-projects">
-									<AllProjects />
+								<div className="bottom">
+									<div className="links">
+										<div className="link">
+											<div className="link-logo">
+												<img
+													src={require("../src/Assets/icons/link-logo.png")}
+													alt="project"
+												/>
+											</div>
+											<a href="https://drive.google.com/file/d/1v16MdAD0AdDj4YeK8HdwukBeRNUGvVDZ/view?usp=drive_link">
+												Resume
+											</a>
+										</div>
+										<div className="link">
+											<div className="link-logo">
+												<img
+													src={require("../src/Assets/icons/link-logo.png")}
+													alt="project"
+												/>
+											</div>
+											<a href="https://www.linkedin.com/in/renzheng/">
+												LinkedIn
+											</a>
+										</div>
+										<div className="link">
+											<div className="link-logo">
+												<img
+													src={require("../src/Assets/icons/link-logo.png")}
+													alt="project"
+												/>
+											</div>
+											<a href="https://github.com/renzheng11">Github</a>
+										</div>
+										<div className="link">
+											<div className="link-logo">
+												<img
+													src={require("../src/Assets/icons/link-logo.png")}
+													alt="project"
+												/>
+											</div>
+											<a href="https://www.are.na/ren-zheng/channels">Are.na</a>
+										</div>
+									</div>
 								</div>
-								<footer>
-									Updated March 2024 <span id="heart">♥</span>
-								</footer>
+							</div>
+							<div className="right-side">
+								<div className="right-wrapper">
+									{/* <div id="project-destination"></div> */}
+									<div className="all-projects">
+										<AllProjects />
+									</div>
+									<footer>
+										Updated March 2024 <span id="heart">♥</span>
+									</footer>
+								</div>
 							</div>
 						</div>
 					</div>
